001d427c8f2f1222f2fed1299d5e7b3512e3ad6d
felipemimoura <felipemimoura@gmail.com>
2021-05-05 16:15:09 -0300

    pegando dados do localstorage

--- src/UserContext.js
+++ src/UserContext.js
@@ -1,7 +1,20 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const UserContext = createContext()
 
-export const UserStorage = ({children}) =>{
-  
+export const UserStorage = ({ children }) => {
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    const patient = JSON.parse(window.localStorage.getItem("patient"))
+    console.log(patient)
+    setData(patient)
+  }, [])
+
+  return (
+    <UserContext.Provider value={data}>
+      {children}
+    </UserContext.Provider>
+  )
+
 }
