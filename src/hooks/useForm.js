cbfd46de5f8afb910de4a49253307ed2e5005af7
felipemimoura <felipemimoura@gmail.com>
2021-05-05 15:20:53 -0300

    criado custom hooks para validação de dados no usuário

---
+++ src/hooks/useForm.js
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+const types = {
+  cpf: {
+    regex: /^\d{3}\.\d{3}\.\d{3}\-\d{2}$/,
+    message: "Preencha um cpf válido"
+  }
+}
+
+const useForm = (type) => {
+  const [value, setValue] = useState("")
+  const [error, setError] = useState(null)
+
+  function validateCPF(value) {
+    if (type === false) return true
+    if (types[type] && !types[type].regex.test(value)) {
+      setError(types[type].message)
+      return false
+    } else {
+      setError(null)
+      return true
+    }
+  }
+
+  function onChange(e) {
+    console.log(e.target.value)
+    if (error) validateCPF(e.target.value)
+
+    if (e.target.type === "checkbox"){
+      setValue(e.target.checked)
+    }else{
+      setValue(e.target.value)
+    }
+
+  }
+  return {
+    value, setValue, error, onChange, onBlur: () => validateCPF(value)
+  }
+}
