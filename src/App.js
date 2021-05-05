76af7a626d7a2b03d22308583572d5470f22b7c6
felipemimoura <felipemimoura@gmail.com>
2021-05-05 15:16:06 -0300

    criado inputs básicos para o formulário

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Form from './components/Form/Index';
 
 function App() {
   return (
     <div className="App">
-    
+      <Form />
     </div>
   );
 }
--- src/components/Form/Index.js
+++ src/components/Form/Index.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Input from '../Inputs/Index'
 
 const Form = () => {
   return (
     <form>
       <h1>Cadastro</h1>
+      <Input label="Nome Completo" type="text" name="fullName" />
+      <Input label="Data de Nascimento" type="date" name="birthdate" />
+      <Input label="CPF" type="text" name="cpf" placeholder="xxx.xxx.xxx-xx" required />
+      <Input label="Usuário Ativo" type="checkbox"  required />
     </form>
   )
 }
